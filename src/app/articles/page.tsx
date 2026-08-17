a802e5e22d7106aec42da0e04354dbf36f1c6e96
scar1377 <scarlettadams77@gmail.com>
2026-08-17 11:12:42 +0100

    add articles and topics routes

---
+++ src/app/articles/page.tsx
@@ -0,0 +1,5 @@
+const ArticlesPage = () => {
+  return <h2>Articles</h2>;
+};
+
+export default ArticlesPage;
---
+++ src/app/topics/page.tsx
@@ -0,0 +1,5 @@
+const TopicsPage = () => {
+  return <h2>Topics</h2>;
+};
+
+export default TopicsPage;
