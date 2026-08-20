cd8968b30152225ed0aa509970f5cbe4a88a0cb7
scar1377 <scarlettadams77@gmail.com>
2026-08-20 16:13:27 +0100

    use context in CommentCard

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -1,21 +1,25 @@
 import type { Comment } from "@/types/api";
 import DeleteButton from "./DeleteButton";
+import useUser from "@/contexts/useUser";
 
 type CommentCardProps = {
   comment: Comment;
   onDeleteSuccess: (id: number) => void;
 };
 const CommentCard = ({ comment, onDeleteSuccess }: CommentCardProps) => {
+  const { user } = useUser();
   return (
     <li>
       <p>{comment.body}</p>
       <p>author: {comment.author}</p>
       <p>created at:{comment.created_at}</p>
       <p>votes:{comment.votes}</p>
-      <DeleteButton
-        comment_id={comment.comment_id}
-        onDeleteSuccess={onDeleteSuccess}
-      />
+      {user?.username === comment.author && (
+        <DeleteButton
+          comment_id={comment.comment_id}
+          onDeleteSuccess={onDeleteSuccess}
+        />
+      )}
     </li>
   );
 };
