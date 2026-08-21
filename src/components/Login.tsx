67732d6d827843ba16030e664850f03b0f722976
scar1377 <scarlettadams77@gmail.com>
2026-08-21 21:04:29 +0100

    add loading status and error handling

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -8,18 +8,29 @@ import { type ChangeEvent, useState } from "react";
 const Login = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
   const { user, setUser } = useUser();
 
   const handleClick = async () => {
     if (user) {
+      setIsLoading(false);
       setIsLoggedIn(false);
       setUser(null);
     } else {
-      setIsLoggedIn(true);
+      setIsLoading(true);
+      setError("");
       const res = await getUsers();
       if (!res.ok) {
+        setError("Oops, something went wrong... Try again later.");
+        setIsLoading(false);
         return;
       }
+      setIsLoggedIn(true);
+      setError("");
+
+      setIsLoading(false);
       setUsers(res.users);
     }
   };
@@ -50,11 +61,18 @@ const Login = () => {
             })}
           </select>
         </>
-      ) : (
+      ) : user ? (
         <>
           <img src={user?.avatar_url} style={{ width: 25, height: 25 }} />
           <span>{user?.username}</span>
           <button onClick={handleClick}>Log out</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleClick}>
+            {isLoading ? "loading..." : "Login"}
+          </button>
+          {error && <p>{error}</p>}
         </>
       )}
     </>
