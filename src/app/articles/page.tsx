b684a795551801eab1026ac8ca7fb7a2bcc30b7c
scar1377 <scarlettadams77@gmail.com>
2026-08-22 22:24:33 +0100

    update Article page to get query values

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,15 +1,23 @@
 import ArticleCard from "@/components/ArticleCard";
-import { Article } from "@/types/api";
+import type { Article } from "@/types/api";
 import { getArticles } from "@/utils/api";
 import Link from "next/link";
 
-const Articles = async () => {
-  const articles = await getArticles();
+type ArticlesProps = {
+  searchParams: Promise<{
+    sort_by?: string;
+    order?: "asc" | "desc";
+    topic?: string;
+  }>;
+};
+const Articles = async ({ searchParams }: ArticlesProps) => {
+  const { sort_by, order, topic } = await searchParams;
+  const articles = await getArticles(sort_by, order, topic);
   return (
     <>
       <h2>Articles</h2>
       <ul>
-        {articles.map((article: Article) => {
+        {articles.map((article) => {
           return (
             <Link
               key={article.article_id}
