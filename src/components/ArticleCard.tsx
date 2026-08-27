ad599ca1a68d5ff36bb7793dfad90373d024a5bd
scar1377 <scarlettadams77@gmail.com>
2026-08-27 22:35:19 +0100

    start styling article card, style the title and topic

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -3,9 +3,11 @@ import { Article } from "@/types/api";
 type ArticleCardProps = { article: Article };
 const ArticleCard = ({ article }: ArticleCardProps) => {
   return (
-    <li>
-      <h3>{article.title}</h3>
-      <p>topic :{article.topic}</p>
+    <li className="rounded-lg border border-orange-100 bg-white p-5 shadow-sm">
+      <h3 className="text-xl font-bold text-orange-900">{article.title}</h3>
+      <span className="rounded-lg border-2 border-orange-200 bg-orange-50 px-2 py-1 text-sm text-orange-500">
+        {article.topic.toUpperCase()}
+      </span>
       <p>author: {article.author}</p>
       <p>created at:{article.created_at}</p>
       <p>votes:{article.votes}</p>
