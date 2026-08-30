ad985644cf1be63fd57bc20bb7dda10514c114b8
scar1377 <scarlettadams77@gmail.com>
2026-08-30 21:49:01 +0100

    add virticle space within ArticleCard && adjust the space between votes and comment count

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -4,25 +4,27 @@ type ArticleCardProps = { article: Article };
 const ArticleCard = ({ article }: ArticleCardProps) => {
   return (
     <li className="rounded-lg border border-orange-100 bg-white p-5 shadow-sm">
-      <h3 className="text-xl font-bold text-orange-900">{article.title}</h3>
-      <span className="rounded-lg border-2 border-orange-200 bg-orange-50 px-2 py-1 text-sm text-orange-500">
-        {article.topic.toUpperCase()}
-      </span>
-      <div className="text-sm text-neutral-500">
-        <span>👤 {article.author}</span>
-        <span> · </span>
-        <span>
-          created at:{" "}
-          {new Date(article.created_at).toLocaleDateString("en-GB", {
-            day: "numeric",
-            month: "short",
-            year: "numeric",
-          })}
+      <div className="flex flex-col gap-3">
+        <h3 className="text-xl font-bold text-orange-900">{article.title}</h3>
+        <span className="rounded-lg border-2 border-orange-200 bg-orange-50 px-2 py-1 text-sm text-orange-500">
+          {article.topic.toUpperCase()}
         </span>
-      </div>
-      <div className="flex items-center justify-between text-sm text-neutral-500">
-        <span>👍🏻 {article.votes} votes</span>
-        <span>💬 {article.comment_count} comments</span>
+        <div className="text-sm text-neutral-500">
+          <span>👤 {article.author}</span>
+          <span> · </span>
+          <span>
+            created at:
+            {new Date(article.created_at).toLocaleDateString("en-GB", {
+              day: "numeric",
+              month: "short",
+              year: "numeric",
+            })}
+          </span>
+        </div>
+        <div className="flex items-center gap-6 text-sm text-neutral-500">
+          <span>👍🏻 {article.votes} votes</span>
+          <span>💬 {article.comment_count} comments</span>
+        </div>
       </div>
     </li>
   );
