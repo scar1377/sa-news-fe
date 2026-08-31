0e70e205a23f8dd496b8a8590d00caa175990dc0
scar1377 <scarlettadams77@gmail.com>
2026-08-31 23:32:27 +0100

    add spacing in Search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -9,6 +9,7 @@ const Search = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const order = searchParams.get("order") ?? "desc";
+  const sortBy = searchParams.get("sort_by") ?? "created_at";
 
   const params = new URLSearchParams(searchParams.toString());
   const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
@@ -22,17 +23,24 @@ const Search = () => {
     router.push(`${pathname}?${params.toString()}`);
   };
   return (
-    <>
+    <div className="flex items-center gap-4">
       <span>Sort by</span>
-      <select onChange={handleChange}>
+      <select
+        onChange={handleChange}
+        defaultValue={sortBy}
+        className="bg-white border-2 border-orange-200 text-orange-500 rounded-md px-3 py-2 hover:border-orange-400 cursor-pointer transition duration-200 text-sm outline-none focus:border-orange-500 focus:ring-2 focus:ring-orange-100"
+      >
         <option value="created_at">date</option>
         <option value="votes">votes</option>
         <option value="comment_count">comments</option>
       </select>
-      <button onClick={handleClick}>
-        {order === "desc" ? "Z->A" : "A->Z"}
+      <button
+        onClick={handleClick}
+        className="bg-orange-50 text-orange-500 px-3 py-2 hover:text-orange-700 cursor-pointer transition duration-200 text-sm outline-orange-500focus:outline-none focus:ring-2 focus:ring-orange-200"
+      >
+        {order === "desc" ? "↓ Desc" : "↑ Asc"}
       </button>
-    </>
+    </div>
   );
 };
 
