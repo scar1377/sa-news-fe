5ac65e3422007babf2b2011a81927e404f96890f
scar1377 <scarlettadams77@gmail.com>
2026-08-31 23:43:10 +0100

    refactor Search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -23,20 +23,24 @@ const Search = () => {
     router.push(`${pathname}?${params.toString()}`);
   };
   return (
-    <div className="flex items-center gap-4">
-      <span>Sort by</span>
-      <select
-        onChange={handleChange}
-        defaultValue={sortBy}
-        className="bg-white border-2 border-orange-200 text-orange-500 rounded-md px-3 py-2 hover:border-orange-400 cursor-pointer transition duration-200 text-sm outline-none focus:border-orange-500 focus:ring-2 focus:ring-orange-100"
-      >
-        <option value="created_at">date</option>
-        <option value="votes">votes</option>
-        <option value="comment_count">comments</option>
-      </select>
+    <div className="flex flex-wrap items-center gap-3">
+      <div className="flex items-center gap-2">
+        <label htmlFor="sort-by">Sort by</label>
+        <select
+          id="sort-by"
+          onChange={handleChange}
+          value={sortBy}
+          className="bg-white border-2 border-orange-200 text-orange-500 rounded-md px-2 py-1.5 sm:px-3 sm:py-2 hover:border-orange-400 cursor-pointer transition duration-200 text-sm outline-none focus:border-orange-500 focus:ring-2 focus:ring-orange-100"
+        >
+          <option value="created_at">date</option>
+          <option value="votes">votes</option>
+          <option value="comment_count">comments</option>
+        </select>
+      </div>
+
       <button
         onClick={handleClick}
-        className="bg-orange-50 text-orange-500 px-3 py-2 hover:text-orange-700 cursor-pointer transition duration-200 text-sm outline-orange-500focus:outline-none focus:ring-2 focus:ring-orange-200"
+        className="bg-orange-50 text-orange-500 px-2 py-1.5 sm:px-3 sm:py-2 hover:text-orange-700 cursor-pointer transition duration-200 text-sm outline-orange-500focus:outline-none focus:ring-2 focus:ring-orange-200"
       >
         {order === "desc" ? "↓ Desc" : "↑ Asc"}
       </button>
