dd93bae1857656f5ad01ac550fe3338c5da3d403
scar1377 <scarlettadams77@gmail.com>
2026-09-08 23:23:39 +0100

    store navLinkClass style into a variable

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,23 +1,21 @@
 import Link from "next/link";
 
 const Nav = () => {
+  const navLinkClass =
+    "mt-4 border-b-2 border-transparent pb-1 font-medium transition duration-200 hover:border-orange-400 hover:text-orange-600";
   return (
     <nav className="flex gap-6 mt-4">
-      <Link
-        href="/"
-        className="mt-4 border-b-2 border-transparent pb-1 font-medium transition duration-200 hover:border-orange-400 hover:text-orange-600"
-      >
+      <Link href="/" className={navLinkClass}>
         Home
       </Link>
-      <Link
-        href="/articles"
-        className="mt-4 border-b-2 border-transparent pb-1 font-medium transition duration-200 hover:border-orange-400 hover:text-orange-600"
-      >
+      <Link href="/articles" className={navLinkClass}>
         Articles
       </Link>
       <Link
         href="/topics"
-        className="mt-4 border-b-2 border-transparent pb-1 font-medium transition duration-200 hover:border-orange-400 hover:text-orange-600"
+        className={
+          "mt-4 border-b-2 border-transparent pb-1 font-medium transition duration-200 hover:border-orange-400 hover:text-orange-600"
+        }
       >
         Topics
       </Link>
