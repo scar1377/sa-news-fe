70c0e87e686aebb81673840d101329deb310fef4
scar1377 <scarlettadams77@gmail.com>
2026-09-08 23:21:21 +0100

    update nav bar to style it more

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,13 +3,22 @@ import Link from "next/link";
 const Nav = () => {
   return (
     <nav className="flex gap-6 mt-4">
-      <Link href="/" className="font-medium hover:text-orange-600">
+      <Link
+        href="/"
+        className="mt-4 border-b-2 border-transparent pb-1 font-medium transition duration-200 hover:border-orange-400 hover:text-orange-600"
+      >
         Home
       </Link>
-      <Link href="/articles" className="font-medium hover:text-orange-600">
+      <Link
+        href="/articles"
+        className="mt-4 border-b-2 border-transparent pb-1 font-medium transition duration-200 hover:border-orange-400 hover:text-orange-600"
+      >
         Articles
       </Link>
-      <Link href="/topics" className="font-medium hover:text-orange-600">
+      <Link
+        href="/topics"
+        className="mt-4 border-b-2 border-transparent pb-1 font-medium transition duration-200 hover:border-orange-400 hover:text-orange-600"
+      >
         Topics
       </Link>
     </nav>
